cd67a300942b4e862ab526f237eadf4c7cdb9649
Student <magnusarbuthnott@gmail.com>
2019-07-15 09:34:53 +0100

    deleted test scenes and renamed corner pillars

---
+++ vr-hide-and-go-leek/Assets/Scripts/ButtonListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListener : MonoBehaviour
+{   
+    public Button FirstButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ vr-hide-and-go-leek/Assets/Scripts/MainCameraInteract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainCameraInteract : MonoBehaviour
+{
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayhit;
+
+            if (Physics.Raycast(mouseray, out rayhit, 1000.0f)) {
+
+                if (rayhit.transform.name == "Cube") {
+                    SceneManager.LoadScene("Main");
+                    // gameObject.active = false;
+                }
+            }
+        }
+    }
+}
